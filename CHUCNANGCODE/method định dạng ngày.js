d5e3dbda5ade9d43fbc6d077da02182b2d932228
Sok Kim Thanh <thanhsk1991@gmail.com>
2017-10-28 11:37:41 +0700

    Thay đổi định dạng tập tin

--- CHUCNANGCODE/method định dạng ngày.js
+++ CHUCNANGCODE/method định dạng ngày.js
@@ -1,9 +1,9 @@
-hàm định dạng ngày form edit
+// hàm định dạng ngày form edit
 if (item.resident_incident_id !== 0){
 	$scope.it.date_time = $filter('date')(new Date(item.date_time), 'dd-MM-yyyy');
 }
 
-hàm định dạng ngày trên form search
+// hàm định dạng ngày trên form search
 $scope.kieu_ngay_gio = function (value) {
             if (value !== "" && value !== null && value !== undefined && value.split(" ")[0] !== "1900-01-01") {
                 var v1 = value.split(" ")[0];
@@ -15,7 +15,7 @@ $scope.kieu_ngay_gio = function (value) {
         }
 <td>{{kieu_ngay_gio(date_time)}}</td>
 
-hàm search ngày từ ngày đến
+// hàm search ngày từ ngày đến
 
 timeout
 id="bill"
---
+++ PMS/WPMS-457/chinhsuabaocaogop_y.js
@@ -0,0 +1,10 @@
+Assignee: "thanh.sok Thanh, Sok";
+Reporter: "khiem.nguyen Khiem, Nguyen";
+
+// Start watching this issue
+var Dates = {
+  Due: "26/Oct/17",
+  Created: "Thursday 10:56 AM",
+  Updated: "Thursday 1:42 PM",
+  Resolved: "Thursday 1:42 PM"
+};
