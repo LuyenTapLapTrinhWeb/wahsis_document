9f706f5ec2d7cc0f6f7edb799b6f345384ff8990
Sok Kim Thanh <thanhsk1991@gmail.com>
2017-12-06 13:42:50 +0700

    .

--- PMS/WPMS-529/Default hình ảnh khi thiết lập rèm.js
+++ PMS/WPMS-529/Default hình ảnh khi thiết lập rèm.js
@@ -10,4 +10,19 @@ var pattern_image_list = [
     // {name: $filter('translate')('WASHING_MACHINE'), value: "washing_machine.jpg"}
     
 ]
+$scope.upload_text = $filter('translate')('UPLOAD')
+$scope.pattern_image_list = []
+for (var i = 1; i <= 18; i++) {
+    $scope.pattern_image_list.push({ name: $filter('translate')('DEVICE_AIRCONDITIONER') + " " + i, value: i + ".png" });
+}
  
+$scope.change_pattern_image = function () {
+    if ($scope.it.pattern_image === "" || $scope.it.pattern_image === undefined || $scope.it.pattern_image === null) {
+        $rootScope.device_image = "assets/img/no_imagesmall.jpg"
+    } else {
+        $rootScope.device_image = "img/device_airconditioner/" + $scope.it.pattern_image;
+        device_image = $rootScope.device_image
+        $scope.item.logo_path = $rootScope.device_image
+    }
+    console.log($rootScope.device_image)
+}
