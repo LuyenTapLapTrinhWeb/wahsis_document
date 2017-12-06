22221ad4b67ec68f3d77c436c328ded1e9d5774e
Sok Kim Thanh <thanhsk1991@gmail.com>
2017-12-06 13:41:20 +0700

    529-Default hinh anh khi thiet lap thiet bi

---
+++ PMS/WPMS-529/Default hình ảnh khi thiết lập rèm.js
@@ -0,0 +1,13 @@
+HI Thanh,
+Em kiểm tra tính năng thiết lập rèm. Vui lòng tìm kiếm hình ảnh default cho rèm.
+
+var pattern_image_list = [
+    // {name: $filter('translate')('LIGHT'), value: "light.jpg"},
+    // {name: $filter('translate')('FAN'), value: "fan.jpg"},
+    // {name: $filter('translate')('AIR_CONDITIONER'), value: "air_conditioner.jpg"},
+    // {name: $filter('translate')('REFRIGERATOR'), value: "refrigerator.jpg"},
+    // {name: "Tivi", value: "tivi.jpg"},
+    // {name: $filter('translate')('WASHING_MACHINE'), value: "washing_machine.jpg"}
+    
+]
+ 
