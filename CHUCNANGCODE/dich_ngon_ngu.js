dedfe20b66543647dc4862bed6758a7c294158df
Sok Kim Thanh <thanhsk1991@gmail.com>
2017-12-08 16:10:22 +0700

    dich ngon ngu thong dung

---
+++ CHUCNANGCODE/dich_ngon_ngu.js
@@ -0,0 +1,21 @@
+ format_from_date: "From date don't match format",
+        format_to_date: "To date don't match format",
+        from_to_date: "To date must greater than from date",
+        check_from_to: "End date must be greater than start date",
+
+        Please_choose_item_to_delete: "Please choose item to delete!",
+        delete_item: "Please choose item to delete!",
+        edit_item: "Please choose item to edit!",
+        email_error: "Email invalidate",
+        date_from_to: "From date and to date invalid",
+        SUCCESS: "Success",
+      WARNING: "Warning",
+      Save_Error: "Save Error",
+      Save_Success: "Save Success",
+      Delete_Error: "Delete Error.",
+      Delete_Success: "Delete Success.",
+      Yes_delete_it: "Yes, delete it!",
+      Are_you_sure: "Are you sure?",
+
+       UPLOAD_ITEM_SUCCESS:"Upload image success",
+      UPLOAD_ITEM_ERROR:"Upload image error"
