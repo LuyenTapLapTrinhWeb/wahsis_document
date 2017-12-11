4999dbc9e87434f46c4d921ac5c51906e404ab72
Sok Kim Thanh <thanhsk1991@gmail.com>
2017-12-11 21:07:20 +0700

    WahSiS PMSWPMS-551 setup master resource

---
+++ PMS/WPMS-551/551setup_master_resource.js
@@ -0,0 +1,4 @@
+Hi Thanh,
+Em vui lòng setup master resource, cho phép người dùng thêm 1 key dùng để phân quyền cho 1 tính năng mới.
+Danh sách resource có phân cấp cha con giống như màn hình phân quyền
+Ví dụ : House keeping có 2 màn hình con
