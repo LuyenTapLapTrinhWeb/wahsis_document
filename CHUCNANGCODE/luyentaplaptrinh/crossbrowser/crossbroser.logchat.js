0b8241ed693d2a6979819a51a6bb70d5e4bb4456
Sok Kim Thanh <thanhsk1991@gmail.com>
2017-12-14 09:29:58 +0700

    crossbrowser tuong thich trinh duyet

---
+++ CHUCNANGCODE/luyentaplaptrinh/crossbrowser/crossbroser.logchat.js
@@ -0,0 +1,20 @@
+[8:45:22 AM] Diep Nguyen: sao mà em biết đc mấ ngườ trc dung cái nào mà h bắt em đi fix lỗi tương thích trình duyệt
+[8:45:33 AM] Diep Nguyen: @@
+[8:46:53 AM] sok thanh: Anh chưa nhận được
+[8:47:24 AM] Diep Nguyen: em cũng ko có thấy
+[8:48:52 AM] Diep Nguyen: Th
+[9:02:25 AM] sok thanh: cross-browser
+[9:02:39 AM] sok thanh: Tương thích trình duyệt
+[9:02:43 AM] Diep Nguyen: dạ anh
+[9:02:51 AM] Diep Nguyen: mà sao ko đưa em cái link đi
+[9:03:13 AM] sok thanh: https://github.com/afarkas/html5shiv
+[9:03:24 AM] Diep Nguyen: okay anh, cám ơn nhiều nhiều nhe
+[9:03:26 AM] Diep Nguyen: =))
+[9:06:20 AM] sok thanh: test thử
+[9:06:34 AM] sok thanh: gõ 1 đống css html5 rule vô
+[9:06:45 AM] sok thanh: rồi them cái này vô trang web
+[9:07:00 AM] sok thanh: thêm cái cross-browser vô trang web
+[9:13:26 AM] sok thanh: https://msdn.microsoft.com/en-us/library/ms537512(v=vs.85).aspx
+[9:13:42 AM] sok thanh: what is conditional comments in html?
+[9:16:51 AM] sok thanh: Cú pháp
+[9:17:12 AM] Diep Nguyen: để em thử
