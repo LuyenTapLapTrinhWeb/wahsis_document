efbd48b5ea3718874c651d68fc1d49c6e46c5edf
Sok Kim Thanh <thanhsk1991@gmail.com>
2017-12-16 17:22:46 +0700

    chia nút khi add hoặc update field ngôn ngữ.

--- CHUCNANGCODE/chia nut bam khi add hoac update.js
+++ CHUCNANGCODE/chia nut bam khi add hoac update.js
@@ -18,3 +18,25 @@ $scope.flag.ngshowCanhoSelect = false;
 } else {
 $scope.flag.ngshowCanhoSelect = true;
 }
+
+/**TRUONG HOP CHI SHOW SELECT LANGUAGE EDIT VA DISABLE LANGUAGE ADD */
+<div ng-show="flag.ngdisableCanhoselect">
+<div class="fg-line">
+    <label class="col-md-2 control-label">{{lang.LANGUAGE_ID}}
+        <span class="c-red">*</span>
+    </label>
+    <div class="col-md-4">
+        <select ng-model="it.language_id" ng-options="x.language_id as x.language_name for x in languages_list" class=" a" ng-disabled="stt_search">
+            <option value="" disabled>--Choose language--</option>
+        </select>
+    </div>
+</div></div>
+
+/**CHIA NUT BAM KHI ADD HOAC UPDATE */
+        $scope.flag = {};
+        if (item.user_guide_id !== 0) {
+            $scope.flag.ngdisableCanhoselect = true;
+            $scope.flag.ngshowCanhoSelect = false;
+        } else {
+            $scope.flag.ngshowCanhoSelect = true;
+        }
