064957c0693a00613d28e047892ccb3321055f2a
Sok Kim Thanh <thanhsk1991@gmail.com>
2018-01-12 14:00:51 +0700

    .

--- CHUCNANGCODE/fix cai bug tree giao dien.js
+++ CHUCNANGCODE/fix cai bug tree giao dien.js
@@ -1,4 +1,5 @@
 Location_Service,$timeout,
+type_list roomTypeList
 /**FIX CAI BUG TREE */
 $scope.is_hide = true
 $scope.change_hide = function () {
@@ -12,7 +13,6 @@ $scope.change_hide = function () {
     }, 50)
 
 }
-
 var thaydoi = function () {
     if ($(".tree_left").hasClass("an")) {
         $(".tree_left").removeClass("an")
@@ -39,6 +39,27 @@ var thaydoi = function () {
 }
 $scope.thaydoi = () => {
     thaydoi();
+}
+var excel = function() {
+    $(document).ready(function () {
+        $("#excel").click(function (e) {
+            e.preventDefault();
+
+            //getting data from our table
+            var data_type = 'data:application/vnd.ms-excel';
+            var table_div = document.getElementById('excelbill');
+            var table_html = table_div.outerHTML.replace(/ /g, '%20');
+
+            var a = document.createElement('a');
+            a.href = data_type + ', ' + table_html;
+            a.download = 'report_excel' + Math.floor((Math.random() * 9999999) + 1000000) + '.xls';
+            a.click();
+            e.preventDefault();
+        });
+    });
+}
+$scope.excel = () => {
+    excel();
 }
 UtilityService.decentralization("pms_report_project_apartment_for_agents").then(function (response) {
 
@@ -201,6 +222,197 @@ $scope.search = function () {
             </div>
         </div>
     </div>
+
+    /** col 5-7 */
+    <div class="ibox-content">
+    <div class="panel-body row">
+        <div class="col-md-1 row show_tree text_doc1 paddRight0" ng-if='is_hide'>
+            <div ng-click="thaydoi()" class='tree_text text_doc'>
+                {{ "FLOOR_PLAN"| translate }}
+            </div>
+        </div>
+        <div class="col-md-3 row tree_left an paddRight0 rightTreeSmallScreen">
+            <div class="col-md-12 title-floor">
+                <div class="row">
+                    {{ "FLOOR_PLAN"| translate }}
+                    <div ng-click="thaydoi()" class="closeicon">
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="well widthfloor">
+                        <div>
+                        </div>
+                        <div id="jstree" js-tree="treeConfig" ng-model="treeData" tree="treeInstance">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12 col-sm-12 col-xs-12 well pull-right content_right">
+            <div class="row flexWrap">
+                <div class="col-md-4 col-sm-6 col-xs-12 truong_search marginBot5">
+                    <div class="row">
+                        <div class="col-md-5 col-sm-5 col-xs-12 ">
+                            <label>
+                                {{ "APARTMENT_TYPE"| translate }}
+                            </label>
+                        </div>
+                        <div class="col-md-7 col-sm-7 col-xs-12">
+                            <select chosen id="states" class="chosen-select width100" ng-model="room_type_id" ng-options="obj.room_type_id as obj.name for obj in type_list"
+                                tabindex="4">
+                                <option selected="" value="">
+                                    {{"ALL"|translate}}
+                                </option>
+                                <option disabled></option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-4 col-sm-6 col-xs-12 truong_search marginBot5">
+                    <div class="row">
+                        <div class="col-md-5 col-sm-5 col-xs-12 ">
+                            <label>
+                                {{ "VIEW_APARTMENT"| translate }}
+                            </label>
+                        </div>
+                        <div class="col-md-7 col-sm-7 col-xs-12">
+                            <select chosen id="states" class="chosen-select width100" ng-model="view_id" ng-options="x.master_data_id as x.master_data_name for x in viewList"
+                                type="text">
+                                <option value="">
+                                    {{"ALL"|translate}}
+                                </option>
+
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12 truong_search marginBot5">
+                    <div class="row">
+                        <div class="col-md-5 col-sm-5 col-xs-12 ">
+                            <label>
+                                {{ "GROUP_CODE"| translate }}
+                            </label>
+                        </div>
+                        <div class="col-md-7 col-sm-7 col-xs-12">
+                            <input type="text" class="form-control" ng-model="agent_code">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12 truong_search marginBot5">
+                    <div class="row">
+                        <div class="col-md-5 col-sm-5 col-xs-12 ">
+                            <label>
+                                {{ "SELLER_GROUP"| translate }}
+                            </label>
+                        </div>
+                        <div class="col-md-7 col-sm-7 col-xs-12">
+                            <select chosen="" class="chosen-select width100" id="states" ng-model="agent_name" ng-options="obj.agent_name  as obj.agent_name for obj in agentsListcbb"
+                                required="true">
+                                <option value="">
+                                    {{"ALL"|translate}}
+                                </option>
+                                <option disabled="">
+                                </option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-xs-12 truong_search marginBot5">
+                    <div class="row">
+                        <div class="col-md-5 col-sm-5 col-xs-12 ">
+                            <label>{{ "FROM_DAY"| translate }}</label>
+                        </div>
+                        <div class="col-md-7 col-sm-7 col-xs-12">
+                            <input type="text" id="fromDate" ng-blur="kiemtradate()" class="form-control" ng-model="fromDate">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12 truong_search marginBot5">
+                    <div class="row">
+                        <div class="col-md-5 col-sm-5 col-xs-12 ">
+                            <label>{{ "TO_DAY"| translate }}</label>
+                        </div>
+                        <div class="col-md-7 col-sm-7 col-xs-12">
+                            <input type="text" id="toDate" class="form-control" ng-blur="kiemtradate()" ng-model="toDate">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-12">
+                <button class="btn btn-primary pull-right" data-ng-click="search()" ng-disabled="allow[1] === false" type="button">
+                    <i class="fa fa-search"></i> {{ "SEARCH"| translate }}
+                </button>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-sm-2 " style="padding-left: 0px;padding-top: 0px; margin-bottom: 5px;padding-right: 0px">
+                <p ng-show="items.length === 0" style="margin-bottom: 0px">
+                    <strong style="color: red">
+                        {{"NO_RESULT"|translate}}
+                    </strong>
+                </p>
+            </div>
+            <div class='col-md-12'>
+                <div class="text-right " id="div_paging">
+                    <button ng-disabled="currentPage1 == 1" ng-click="load_data(4)" class="btn btn-default btn-xs black">
+                        <i class="ion-arrow-left-a"></i>
+                        {{ "FIRST"| translate }}
+                    </button>
+                    <button ng-disabled="currentPage1 == 1" ng-click="load_data(1)" class="btn btn-default btn-xs black">
+                        <i class="ion-chevron-left"></i>
+                        {{ "PREVIOUS"| translate }}
+                    </button>
+                    {{ "ITEM_PER_PAGE"| translate }}:
+                    <select id="page" ng-model="item_per_page" ng-change="item_per_page2()" ng-options="x.value as x.text for x in item_per_page1"></select>
+                    {{ "PAGE"| translate }}
+                    <select id="page" class="page" ng-model="currentPage1" ng-change="load_data(2)" ng-options="x.val as x.test for x in list_page"></select>
+                    {{ "TOTAL_PAGE"| translate }}: {{ total_page }}
+                    <button ng-disabled="currentPage1 == total_page || total_page == 0" ng-click="load_data(3)" class="btn btn-default btn-xs black">
+                        <i class="ion-chevron-right"></i>
+                        {{ "NEXT"| translate }}
+                    </button>
+                    <button ng-disabled="currentPage1 == total_page || total_page == 0" ng-click="load_data(5)" class="btn btn-default btn-xs black">
+                        <i class="ion-arrow-right-a"></i>
+                        {{ "LAST"| translate }}
+                    </button> | {{ "TOTAL_RESULT"| translate }}: {{ total_row }}
+                </div>
+            </div>
+            <div class='col-md-12'>
+                <div class="table-responsive" style="overflow-x: auto; ">
+                    <table style="display: table;overflow-x: inherit; max-height: 300px; min-width:100%" data-page-length='{{page_length}}' datatable="ng"
+                        dt-options="dtOptions" class="table table-striped table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th style="white-space: nowrap;">{{"STT"|translate}}</th>
+                                <th style="white-space: nowrap;">{{"GROUP_CODE"|translate}}</th>
+                                <th style="white-space: nowrap;">{{"SELLER_GROUP"|translate}}</th>
+                                <th style="white-space: nowrap;">{{"TOTAL_SALE"|translate}}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr ng-repeat="item in userList">
+                                <td style="white-space: nowrap;">{{item.stt}}</td>
+                                <td style="white-space: nowrap;">{{item.agent_code}}</td>
+                                <td style="white-space: nowrap;">{{item.agent_name}}</td>
+
+                                <td style="white-space: nowrap; text-align: right;font-weight: normal ">{{kieu(item.total_sales)}}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    /** col 4-8 */
     <div class="ibox-content">
         <div class="panel-body row" style="padding-top:0px">
             <div class="col-md-1 row show_tree text_doc1" style="padding-right: 0px;" ng-if='is_hide'>
