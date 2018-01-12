3f30f12c3ff412f122a951c7e35a2bcaec923d41
Sok Kim Thanh <thanhsk1991@gmail.com>
2018-01-12 21:57:51 +0700

    .

--- CHUCNANGCODE/FIX CAI BUG THIET LAP MENU.js
+++ CHUCNANGCODE/FIX CAI BUG THIET LAP MENU.js
@@ -54,21 +54,27 @@ pms_setting_log_database_pms_web
   ];
 ============================================
 -----navigation.html
-<li ng-if="main_menu[6].is_show && showMainSubmenu(main_menu, 'notification', 'log_database_pms_web')" ng-class="{active: $state.includes('log_history_pmsweb')}">
+<!-- /**
+Ghi chú: 
+MAIN_MENU = notification = TOP MENU  Đã đăng ký trong file config
+MAIN_SUB_MENU = STATE CHA = notifications_logdbpmsweb = LEFT MENU Đã đăng ký trong file config
+SUB_MENU =  STATE cha(.)STATE CON = ui-sref="notifications_logdbpmsweb.log_database_pms_web" = SUB MENU Đã đăng ký trong file config
+MAIN_MENU_ID  = showMainSubmenu(main_menu, 'notification', 'log_database_pms_web') showMainSubmenu = function (main_menu, id, sub_id)
+SUB_MENU_ID = showSubmenu(main_menu, 'notification', 'log_database_pms_web', 'pms_setting_log_database_pms_web') showSubmenu = function (main_menu, id, sub_id, child_id)
+MODULE_ID = pms Không đổi được phải xóa đi tạo lại vì vậy cần viết chính xác module_id
+RESOURCE_ID = pms_setting_log_database_pms_web
+**/ -->
+<li ng-if="main_menu[0].is_show && showMainSubmenu(main_menu, 'top_menu_dashboard', 'left_menu_dashboard')" ng-class="{active: $state.includes('state_cha_dashboard')}">
 <a href="">
     <i class="widthIcon fa fa-clock-o"></i>
-    <span class="nav-label menu_parent">{{"LOG_DATABASE_PMS_WEB"| translate}}</span>
+    <span class="nav-label menu_parent">{{"GENERAL"| translate}}</span>
     <span class="fa arrow"></span>
 </a>
-/**
------MODULE_ID = PMS Không đổi được phải xóa đi tạo lại vì vậy cần viết chính xác module_id
------RESOURCE_ID = PMS_SETTING_LOG_DATABASE_PMS_WEB
-*/
-<ul class="nav nav-second-level collapse" ng-class="{in: $state.includes('notifications_logdbpmsweb')}">
-    <li ui-sref-active="active" ng-if="showSubmenu(main_menu, 'notification', 'log_database_pms_web', 'pms_setting_log_database_pms_web')">
-        <a ui-sref="notifications_logdbpmsweb.log_database_pms_web" ng-click="capnhat_id_user_guide('pms_setting_log_database_pms_web','pms')">
-            <i class="widthIcon fa fa-clock-o" aria-hidden="true" ></i>
-            <span class="nav-label">{{"LDPMSW_MODULE"| translate}}</span>
+<ul class="nav nav-second-level collapse" ng-class="{in: $state.includes('state_cha_dashboard')}">
+    <li ui-sref-active="active" ng-if="showSubmenu(main_menu, 'top_menu_dashboard', 'left_menu_dashboard', 'sub_menu_dashboard')">
+        <a ui-sref="state_cha_dashboard.state_con_dashboard" ng-click="capnhat_id_user_guide('pms_dashboard_total_value','sms')">
+            <i class="widthIcon fa fa-clock-o" aria-hidden="true"></i>
+            <span class="nav-label">{{"TOTAL_VALUE"| translate}}</span>
         </a>
     </li>
 </ul>
