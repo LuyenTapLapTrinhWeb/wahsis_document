761f7b56956f0143c943b7fb4b867330ffb4791c
Sok Kim Thanh <thanhsk1991@gmail.com>
2018-03-01 13:01:31 +0700

    .

---
+++ CHUCNANGCODE/fix cai bug tao tab.js
@@ -0,0 +1,15 @@
+<div class="modal-body">
+        <div class="tabs-container">
+            <form class="row" name="frm" novalidate>
+                <uib-tabset class="row" active="active" style="  overflow: hidden;">
+                    <uib-tab heading="{{'INFORMATION'|translate}}" index="0" select="tabSelect()">
+
+                        
+                    </uib-tab>
+                    <uib-tab heading='{{"IMAGE_LIST"|translate}}' index="1" select="tabSelect2()">
+                         
+                        </uib-tab>
+                </uib-tabset>
+            </form>
+        </div>
+    </div>
