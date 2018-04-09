d09752f0bdb853d30f4b0fc0621d3a80ca8b89b9
Sok Kim Thanh <thanhsk1991@gmail.com>
2018-04-09 14:50:13 +0700

    .

---
+++ CHUCNANGCODE/fix cai bug loading timeout ba cham.js
@@ -0,0 +1,17 @@
+<div class="spiner-example" ng-show="is_load">
+    <div class="sk-spinner sk-spinner-three-bounce">
+        <div class="sk-bounce1"></div>
+        <div class="sk-bounce2"></div>
+        <div class="sk-bounce3"></div>
+    </div>
+</div>
+UtilityService.decentralization("pms_project_deposit").then(function (response) {
+    if (response.data.err === 0 && response.data.dt !== undefined) {
+        $scope.is_load = true;
+        $scope.load_data = () => {
+            if (response.err === 0) {
+                $scope.is_load = false;
+            }
+        }
+    }
+})
