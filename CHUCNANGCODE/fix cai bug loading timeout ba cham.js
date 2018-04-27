5f48c96dc0f4e4ffe9846cfb9c6806903972115d
Sok Kim Thanh <thanhsk1991@gmail.com>
2018-04-28 06:48:45 +0700

    update class loading time out

--- CHUCNANGCODE/fix cai bug loading timeout ba cham.js
+++ CHUCNANGCODE/fix cai bug loading timeout ba cham.js
@@ -1,3 +1,14 @@
+.spiner-example {
+    height: 107.5%;
+    width: 100%;
+
+    position: absolute;
+    top: 0%;
+    left: 0%;
+    z-index: 10000;
+    background: black;
+    opacity: 0.6;
+}
 <div class="spiner-example" ng-show="is_load">
     <div class="sk-spinner sk-spinner-three-bounce">
         <div class="sk-bounce1"></div>
