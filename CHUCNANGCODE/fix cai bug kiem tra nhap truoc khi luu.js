1730af99dd23f042479349adc73801008839f39a
Sok Kim Thanh <thanhsk1991@gmail.com>
2018-09-20 13:56:19 +0700

    update

--- CHUCNANGCODE/fix cai bug kiem tra nhap truoc khi luu.js
+++ CHUCNANGCODE/fix cai bug kiem tra nhap truoc khi luu.js
@@ -5,6 +5,12 @@ $scope.kiemtra_nhap_truockhi_luu = () => {
     else if (!utility.checkValue($scope.model_lock)) { swal($filter("translate")("Save_Error"), $filter("translate")("c_red"), "warning"); duoc_di_tiep = false; }
     else if (!utility.checkValue($scope.lock_name)) { swal($filter("translate")("Save_Error"), $filter("translate")("c_red"), "warning"); duoc_di_tiep = false; }
     else if (utility.checkArrayLength($scope.new_array_img)) { swal($filter("translate")("Save_Error"), $filter("translate")("c_red"), "warning"); duoc_di_tiep = false; }
+
+    /* time */
+    else if ($scope.kiemtra_gio($scope.item.from_time) === false) { swal($filter("translate")("Save_Error"), $filter("translate")("from_time_invalid"), "warning"); duoc_di_tiep = false; }
+    else if ($scope.kiemtra_gio($scope.item.to_time) === false) { swal($filter("translate")("Save_Error"), $filter("translate")("to_time_invalid"), "warning"); duoc_di_tiep = false; }
+    else if ($scope.sosanh_gio($scope.item.from_time, $scope.item.to_time) === -1) { swal($filter("translate")("Save_Error"), $filter("translate")("from_to_time_invalid"), "warning"); duoc_di_tiep = false; }
+
     else { duoc_di_tiep = true; }
     if (duoc_di_tiep) {
         return true;
