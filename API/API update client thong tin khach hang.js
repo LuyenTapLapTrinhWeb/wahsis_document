b375f98f845f2482272d1fe27628581fcdb660d3
Sok Kim Thanh <thanhsk1991@gmail.com>
2019-02-20 08:42:11 +0700

    update

---
+++ API/API update client thong tin khach hang.js
@@ -0,0 +1,14 @@
+Request URL: http://pms-dev.wahsis.net/api/apartment_sales_client_contact?
+Request Method: POST
+Status Code: 200 OK
+Remote Address: 125.212.211.168:80
+Referrer Policy: no-referrer-when-downgrade
+Provisional headers are shown
+Accept: application/json, text/plain, */*
+Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJ2ZXIiOiIwLjEiLCJhaWQiOiIxMDAwMiIsImF0aSI6IjEiLCJtaWQiOiI1NDc0NCIsInVzciI6ImFkbWluIiwicm9sIjoic3RhZmYiLCJpYXQiOjE1NTAxMjQyMjYsImp0aSI6InBkaGFmMXcwNnUyMnJqMG4ifQ.Riiw-UTrMysZ8YGo9QrXORY2VY2E5CebRE3n2Qy7x-8
+Content-Type: application/x-www-form-urlencoded
+Origin: http://localhost:3000
+Referer: http://localhost:3000/
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36
+cm: update
+dt: {"apartment_sales_client_contact":{"apartment_sales_client_contact_id":0,"users_id":2,"apartment_sales_client_id":41024},"company":{"company_id":54744}}
