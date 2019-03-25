0d7ca96a21b8181d9147971058345c104c85c560
Sok Kim Thanh <thanhsk1991@gmail.com>
2019-03-25 08:40:20 +0700

    update chuc nang thao tac project

---
+++ CAUHINHPROJECT/cach add room vao map location.js
@@ -0,0 +1,15 @@
+B1: tạo căn => vs Mã căn phải lấy tên khu vực DO201 vì trên map đang đánh mã như vậy để hiểu ô đó là căn DO2-01
+    1.1 Bước chọn menu: thiết lập> dự án> căn
+    1.2 Bước nhập dữ liệu
+    Mã căn: DO1106 
+    Tên: DO11-06 
+    Khu vực: DO11 
+    Sắp sếp:  6 
+    Diện tích tim tường: 3 
+    số Diện tích thông thủy: 3 số
+    Trạng thái: khóa
+    Lưu
+Bước 2: vào trang sản phẩm cần bán => thêm căn đó vào chiến dịch mở bán tháng 7
+Bước 3: Vào trang hoa hồng nhân viên , chọn chiến dịch mở bán tháng 7 => Thêm căn đó vào cho Team A
+
+DO4-02 => Mã là DO402 => Tên là DO4-02
