d5b35d99ae031a7c5099e55a98129c45e6f0b264
Sok Kim Thanh <thanhsk1991@gmail.com>
2019-04-19 11:26:22 +0700

    update api list apartment_sales_marketing

---
+++ API/api list apartment_sales_marketing.js
@@ -0,0 +1,7 @@
+Request URL: http://pms-dev.wahsis.net/api/apartment_sales_marketing?
+Request Method: POST
+Status Code: 200 OK
+Remote Address: 125.212.211.168:80
+Referrer Policy: no-referrer-when-downgrade
+cm: list
+dt: {"company":{"company_id":54744},"languages":{"language_id":"vi"},"apartment_sales_marketing":{"from_date":""}}
