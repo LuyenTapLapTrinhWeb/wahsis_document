61d5135a8b98c1427cbbd7f6c1b913562e8a2653
Sok Kim Thanh <thanhsk1991@gmail.com>
2019-06-12 09:04:00 +0700

    tinh tien duoc giam gia

--- CHUCNANGCODE/tinh tien giam gia dat com now.vn.js
+++ CHUCNANGCODE/tinh tien giam gia dat com now.vn.js
@@ -9,3 +9,15 @@ let tien_duoc_giam_cho_mot_nguoi = tong_tien_dat_com - ((dat_toi_thieu - (giam_g
 check_dieu_kien_giam_toi_da(giam_toi_da, tien_duoc_giam_cho_mot_nguoi){
     if (tien_duoc_giam_cho_mot_nguoi < giam_toi_da) { return true; } else { return false; }
 }
+
+function get_tien_duoc_giam(tong_tien_dat_com_input, dat_toi_thieu_input, giam_gia_input, giam_toi_da_input, so_luong_input) {
+    let tong_tien_dat_com = tong_tien_dat_com_input,
+        dat_toi_thieu = dat_toi_thieu_input,
+        giam_gia = giam_gia_input / 100,
+        giam_toi_da = giam_toi_da_input,
+        so_luong = so_luong_input;
+    let tien_duoc_giam_cho_mot_nguoi = tong_tien_dat_com - ((dat_toi_thieu - (giam_gia * dat_toi_thieu)) / so_luong);
+    return tien_duoc_giam_cho_mot_nguoi;
+}
+let tien_duoc_giam_cho_mot_nguoi = get_tien_duoc_giam(35000, 70000, 25, 50000, 2)
+console.log("tien_duoc_giam_cho_mot_nguoi", tien_duoc_giam_cho_mot_nguoi)
