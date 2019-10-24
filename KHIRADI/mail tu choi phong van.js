9ab3bb643f7df4d608678667c16de8765567a9a3
Sok Kim Thanh <thanhsk1991@gmail.com>
2019-10-24 13:40:48 +0700

    update code

---
+++ KHIRADI/mail tu choi phong van.js
@@ -0,0 +1,8 @@
+/* Tu choi phong van */
+`I am very happy to receive your invitation to interview at the Front-End position. However, I regret to inform you that I was unable to attend the interview on October 24, 2019 for personal reasons.
+Hope we have the opportunity of long-term cooperation on another occasion.
+
+Once again, I would like to sincerely thank you for your invitation to interview.
+
+Best regards.`
+ 
---
+++ KHIRADI/mail xin tro cap Applying for Financial Aid.js
@@ -0,0 +1,13 @@
+/* Why are you applying for Financial Aid? (150 words minimum required) */
+`To: Nanjing University professors,
+From: Sok Kim Thanh
+Subject: Applying for Financial Aid Nanjing University Software Testing course
+Dear Professors,
+I am applying for this course because I really don't have enough money to pay for it. In addition to the money to sustain my life and housing, I would like to have more money to invest in my own favorite course like this one. But I can't do it now. Please review and subsidize the professors. 
+Regards,
+Sok Kim Thanh`
+/* How will taking this course help you achieve your career goals? (150 words minimum required) */
+`Dear Professors
+I would answer the question: "How will taking this course help you achieve your career goals?"
+If I complete this course, I will have a better understanding of software testing. I am a programmer myself and I want to improve my code writing experience.
+Thanks very much`
