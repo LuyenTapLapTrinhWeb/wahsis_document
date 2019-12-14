a389afc496c5e035889ccec4087adf96f8090331
Sok Kim Thanh <thanhsk1991@gmail.com>
2019-12-14 08:51:43 +0700

    update

---
+++ KHIRADI/mail thu thay doi lich phong van.js
@@ -0,0 +1,9 @@
+`Dear Ms. Ngọc Hiền,
+I am pleased to receive your invitation to interview at the Front-End position. 
+However, I regret to inform you that I was unable to attend the interview on November 29, 2019, for personal reasons. 
+I will be going to join in the interview on December 12, 2019, at 1:30 PM.
+I hope we have the opportunity for long-term cooperation. 
+Once again, I would like to thank you for your invitation to interview. 
+Best regards,
+Sok Kim Thanh | Front-End Developer(AngularJS)
+P: 0924964226.`
