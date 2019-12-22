5bf39c1488f1ceaf5b2414207c9c17bfbec0780e
Sok Kim Thanh <thanh.sok@wahsis.com>
2019-12-22 17:50:20 +0700

    update

---
+++ DIPHONGVAN/ontapcss.js
@@ -0,0 +1,22 @@
+css_bi_quen = [
+    { parent: { name: "css selectors", css_khong_lam_duoc: [{}] } },
+    { parent: { name: "css howto", css_khong_lam_duoc: [{}] } },
+    {
+        parent: {
+            name: "css backgournd",
+            css_khong_lam_duoc: [
+                { name: "background-image", css_khong_lam_duoc: [{}] },
+                { name: "background-position", css_khong_lam_duoc: [{}] },
+                { name: "background-repeat", css_khong_lam_duoc: [{}] },
+                { name: "background short hand", css_khong_lam_duoc: [{}] },
+            ]
+        }
+    },
+    {
+        parent: {
+            name: "css border", css_khong_lam_duoc: [
+                { name: "border-width", css_khong_lam_duoc: [{}] },
+            ]
+        }
+    },
+]
