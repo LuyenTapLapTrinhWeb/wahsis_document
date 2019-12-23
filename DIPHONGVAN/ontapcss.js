36ff7d081a963115b1ab7bb2748e6aa0e27e1045
Sok Kim Thanh <thanhsk1991@gmail.com>
2019-12-23 10:56:18 +0700

    margin padding

--- DIPHONGVAN/ontapcss.js
+++ DIPHONGVAN/ontapcss.js
@@ -1,22 +1,42 @@
 css_bi_quen = [
-    { parent: { name: "css selectors", css_khong_lam_duoc: [{}] } },
-    { parent: { name: "css howto", css_khong_lam_duoc: [{}] } },
     {
-        parent: {
-            name: "css backgournd",
-            css_khong_lam_duoc: [
-                { name: "background-image", css_khong_lam_duoc: [{}] },
-                { name: "background-position", css_khong_lam_duoc: [{}] },
-                { name: "background-repeat", css_khong_lam_duoc: [{}] },
-                { name: "background short hand", css_khong_lam_duoc: [{}] },
-            ]
-        }
+        name: "css selectors", css_khong_lam_duoc: [
+            { name: "element selector", css_khong_lam_duoc: [{}] },
+            { name: "id selector", css_khong_lam_duoc: [{}] },
+            { name: "class selector", css_khong_lam_duoc: [{}] },
+            { name: "group selector", css_khong_lam_duoc: [{}] },
+        ]
     },
     {
-        parent: {
-            name: "css border", css_khong_lam_duoc: [
-                { name: "border-width", css_khong_lam_duoc: [{}] },
-            ]
-        }
+        name: "css howto", css_khong_lam_duoc: [
+            { name: "css external style (link)", css_khong_lam_duoc: [{}] },
+            { name: "css internal style(head>style", css_khong_lam_duoc: [{}] },
+            { name: "css inline style (body style)", css_khong_lam_duoc: [{}] },
+        ]
+    },
+    {
+        name: "css backgournd", css_khong_lam_duoc: [
+            { name: "background-color", css_khong_lam_duoc: [{}] },
+            { name: "background-image", css_khong_lam_duoc: [{}] },
+            { name: "background-position", css_khong_lam_duoc: [{}] },
+            { name: "background-repeat", css_khong_lam_duoc: [{}] },
+            { name: "background short hand", css_khong_lam_duoc: [{}] },
+        ]
+    },
+    {
+        name: "css border", css_khong_lam_duoc: [
+            { name: "border-width", css_khong_lam_duoc: [{}] },
+            { name: "border-style", css_khong_lam_duoc: [{}] },
+            { name: "border-color", css_khong_lam_duoc: [{}] },
+            { name: "border-short hand", css_khong_lam_duoc: [{}] },
+        ]
+    },
+    {
+        name: "css margin", css_khong_lam_duoc: [
+            { name: "margin-left", css_khong_lam_duoc: [{}] },
+            { name: "margin-all", css_khong_lam_duoc: [{}] },
+            { name: "margin-property to set", css_khong_lam_duoc: [{}] },
+            { name: "margin-property to center", css_khong_lam_duoc: [{}] },
+        ]
     },
 ]
