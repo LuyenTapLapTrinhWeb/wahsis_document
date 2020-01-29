07f8a28b7fe59a06635443beb6b9f7e00e0edc8a
thanhsok <thanhsk1991@gmail.com>
2020-01-29 14:24:44 +0700

    asset-config

---
+++ CAUHINHPROJECT/cach cau hinh angular json global style javascript.js
@@ -0,0 +1,28 @@
+buoc 1: doc lai trang nay https://angular.io/guide/workspace-config#asset-config
+buoc 2: sang buoc 3
+buoc 3: xem cach go terminal them jquery https://www.gurustop.net/blog/2016/11/18/3821/jquery-angular2-angularcli-how-to
+buoc 4: cai dat angular.json 
+// "assets": [
+//     {
+//       "glob": "**/*",
+//       "input": "src/assets/",
+//       "output": "/assets/"
+//     },
+//     {
+//       "glob": "favicon.ico",
+//       "input": "src/",
+//       "output": "/"
+//     }
+//   ],
+//   "styles": [
+//     {
+//       "input": "src/styles.css",
+//       "inject": false
+//     }
+//   ],
+//   "scripts": [
+//     {
+//       "input": "node_modules/jquery/dist/jquery.min.js",
+//       "inject": false
+//     }
+//   ]
