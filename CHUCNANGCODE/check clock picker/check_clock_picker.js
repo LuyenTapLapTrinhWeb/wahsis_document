45cc6b0f62781e2d29d0ec9d089539fe6a6e9a04
Sok Kim Thanh <thanhsk1991@gmail.com>
2020-03-24 10:31:36 +0700

    update check clock picker

--- CHUCNANGCODE/check clock picker/check_clock_picker.js
+++ CHUCNANGCODE/check clock picker/check_clock_picker.js
@@ -1,2 +1,36 @@
 $scope.to_time = item.to_time;
 $scope.scheduler_name = item.scheduler_name;
+$('.clockpicker').clockpicker()
+    .find('input').change(function () {
+        // TODO: time changed
+        console.log(this.value);
+    });
+$('#demo-input').clockpicker({
+    autoclose: true
+});
+
+if (something) {
+    // Manual operations (after clockpicker is initialized).
+    $('#demo-input').clockpicker('show') // Or hide, remove ...
+        .clockpicker('toggleView', 'minutes');
+}
+
+
+var input = $('#input-a');
+input.clockpicker({
+    autoclose: true
+});
+
+// Manual operations
+$('#button-a').click(function (e) {
+    // Have to stop propagation here
+    e.stopPropagation();
+    input.clockpicker('show')
+        .clockpicker('toggleView', 'minutes');
+});
+$('#button-b').click(function (e) {
+    // Have to stop propagation here
+    e.stopPropagation();
+    input.clockpicker('show')
+        .clockpicker('toggleView', 'hours');
+});
