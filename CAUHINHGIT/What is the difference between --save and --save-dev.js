3091f05d8fe0632a3fc23b35862693908e3f5aaa
Sok Kim Thanh <thanhsk1991@gmail.com>
2020-07-29 15:27:23 +0700

    what is different between --save and --save-dev?

---
+++ CAUHINHGIT/What is the difference between --save and --save-dev.js
@@ -0,0 +1,10 @@
+`What is the difference between:
+
+npm install [package_name] --save
+and:
+
+npm install [package_name] --save-dev
+What does this mean?`
+
+/* --save-dev is used to save the package for development purpose. Example: unit tests, minification..
+--save is used to save the package required for the application to run. */
